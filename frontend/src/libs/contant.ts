6fbcbfff32984a52bdfa475353f05ebd7f3e0fd6
0xcodevil <214421686+0xcodevil@users.noreply.github.com>
2024-10-10 02:34:58 -0700

    Add packages

---
+++ frontend/src/libs/contant.ts
@@ -0,0 +1,46 @@
+export const LINK = {
+    TELEGRAM_MINIAPP: 'https://t.me/CatInTheFishBot',
+    TELEGRAM_GROUP: 'https://t.me/tapps_bot',
+    TELEGRAM_CHANNEL: 'https://t.me/Catinthefish',
+    X: 'https://x.com/CatInTheFish?t=p9OEN2s6HczzGOlhMc2xyw&s=09',
+    YOUTUBE: 'https://youtube.com/@test',
+    WEBSITE: 'https://test.xyz/',
+    TWEET: 'https://x.com/tapp?t=w9mTEnT0h7M7zzj9ki4jpw&s=09',
+    MANIFEST: 'https://supercoolx.github.io/manifests/catfish-manifest.json',
+}
+
+export const ROLE = {
+    ADMIN: 'admin',
+    USER: 'user'
+}
+
+export const GAME = {
+    "easy": {
+        FALL_TIME: 3000,
+        FALL_INTERVAL: 300,
+        FROZEN_TIME: 2000,
+        DURATION: 30 * 1000
+    },
+    "medium": {
+        FALL_TIME: 2000,
+        FALL_INTERVAL: 200,
+        FROZEN_TIME: 1500,
+        DURATION: 60 * 1000
+    },
+    "hard": {
+        FALL_TIME: 1000,
+        FALL_INTERVAL: 100,
+        FROZEN_TIME: 1000,
+        DURATION: 3 * 60 * 1000
+    },
+}
+
+export const OWNER_ADDRESS = "UQADlThF2_zCkpo_MYhO7IXUeO31ooNLB_A_SMcZHdAKf5qk";
+export const IS_MAINNET = true;
+
+export const PLATFORM = {
+    X: 'X',
+    YOUTUBE: 'YouTube',
+    WEBSITE: 'Site',
+    TWEET: 'Tweet'
+}
